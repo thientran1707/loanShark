34a149d1827ecad7c47999661e1841f6ca83c56c
Khoa Phan <khoaphan89@gmail.com>
2015-09-26 12:48:02 -0700

    Add node handling of static bower components and create .gitignore (add node_modules to .gitignore)

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
