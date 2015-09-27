ecc2828e5fbd35a54a14fd8f8492e8633b98e697
Khoa Phan <khoaphan89@gmail.com>
2015-09-27 12:16:01 -0700

    Working update profile

--- client/www/js/controllers/ProfileCtrl.js
+++ client/www/js/controllers/ProfileCtrl.js
@@ -14,29 +14,26 @@ app.controller("ProfileCtrl", [
 			id: Session.getSession()._id,
 		};
 
-		$scope.updateUser = function(user) {
-			Friends.update(user).then(function(data){
-				 // An alert dialog
-			   $ionicPopup.alert({
-			     title: 'User updated successfully!'
-			   });
-			   alertPopup.then(function(res) {
-			     $("body").removeClass("popup-open");
-			     $(".backdrop").css("visibility", "hidden");
-			     $(".popup-container").remove();
-			   });
-			});
-		};
+		// $scope.updateUser = function(user) {
+		// 	Friends.update(user).then(function(data){
+		// 		 // An alert dialog
+		// 	   $ionicPopup.alert({
+		// 	     title: 'User updated successfully!'
+		// 	   });
+		// 	   alertPopup.then(function(res) {
+		// 	     $("body").removeClass("popup-open");
+		// 	     $(".backdrop").css("visibility", "hidden");
+		// 	     $(".popup-container").remove();
+		// 	   });
+		// 	});
+		// };
 
-		$scope.updateUserTest = function() {
-			console.log('testing');
-			console.log("session data is ", Session.getSession());
+		$scope.updateUser = function() {
+			console.log('$scope.userInfo', $scope.userInfo);
 			$http.post('http://localhost:3000/users/updateInfo', $scope.userInfo).then(function(data) {
 				console.log('data is data', data);
 			});
 		};
-
-		$scope.updateUserTest();
 
 	}
 ]);
